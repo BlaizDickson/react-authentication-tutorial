1a3750f27500ae1c6859edd53c6bba8c0fc679b8
BlaizDickson <bakeungdickson@gmail.com>
2023-10-18 22:36:11 +0100

    setup routing in main.js

--- react-auth-app/src/main.jsx
+++ react-auth-app/src/main.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import Login from './Login';
+import Signup from './Signup';
+import Dashboard from './Dashboard';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Initialize Firebase
+const firebaseConfig = {
+  // Your Firebase configuration values
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
+
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
